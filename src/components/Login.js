094f9d22aadb997ca4a329ea7fd5a94331d669e2
amitnandan <a.amitnandan@gmail.com>
2023-11-04 02:11:56 +0530

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
   const [isSignINForm, setIsSignInForm] = useState();
+  const email = useRef(null);
+  const password = useRef(null);
+
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignINForm);
+  };
+
+  const handleButtonClick = () => {
+    //validate the form data
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value
+    );
+    if (message != null) return <></>;
   };
 
   return (
@@ -16,7 +29,10 @@ const Login = () => {
           alt="Logo"
         />
       </div>
-      <form className=" w-3/12 p-12 bg-black absolute my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className=" w-3/12 p-12 bg-black absolute my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
         <h1 className="font-bold text-2xl py-4">
           {isSignINForm ? "Sign In" : "SignUp"}
         </h1>
@@ -29,17 +45,22 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="email Address"
           className="p-2 my-4 w-full bg-gray-400 placeholder-gray-700"
         />
 
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-4 w-full bg-gray-400 placeholder-gray-700"
         />
-        <button className="p-4 my-6 rounded-lg bg-red-800 w-full">
+        <button
+          className="p-4 my-6 rounded-lg bg-red-800 w-full"
+          onClick={handleButtonClick}
+        >
           {isSignINForm ? "Sign In" : "SignUp"}
         </button>
         <p className="py-6 cursor-pointer" onClick={toggleSignInForm}>
