5b1f65fde67c36850d76a16785f8c15a47520af5
amitnandan <a.amitnandan@gmail.com>
2023-11-10 16:37:53 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, USER_AVATAR } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Header = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -31,34 +32,29 @@ const Header = () => {
         navigate("/");
       }
     });
+
+    //called when component unmounts
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
-        // Sign-out successful.
         navigate("/");
       })
       .catch((error) => {
-        // An error happened.
         navigate("/error");
       });
   };
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="Logo"
-      />
+      <img className="w-44" src={LOGO} alt="Logo" />
       {user && (
         <div className="flex p-2">
-          <img
-            className="w-12 h-12 "
-            alt="usericon"
-            src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
-          />
+          <img className="w-12 h-12 " alt="usericon" src={USER_AVATAR} />
           <button
             onClick={handleSignOut}
             className="font-bold text-white rounded-lg"
