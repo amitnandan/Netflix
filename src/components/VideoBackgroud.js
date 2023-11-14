0e2d3a44d02e6c46feeb36738dbed0a106170a8e
amitnandan <a.amitnandan@gmail.com>
2023-11-14 17:11:02 +0530

    VideoContainer done

--- src/components/VideoBackgroud.js
+++ src/components/VideoBackgroud.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
-const VideoBackgroud = () => {
-  return <div></div>;
+const VideoBackgroud = ({ movieId }) => {
+  //fetch trailer video
+
+  useMovieTrailer(movieId);
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  return (
+    <div className="">
+      <iframe
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?si=vJPM8kXxsTwd8WaD&autoplay=1&mute=1"
+        }
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackgroud;
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-20">
+    <div className=" w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black  ">
       <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/4 ">{overview}</p>
+      <p className="py-6 text-lg w-1/4 e ">{overview}</p>
       <div className="">
-        <button className="bg-gray-500 text-white p-2 px-16 text-lg rounded-md bg-opacity-60 ">
+        <button className="bg-white text-black p-2 px-16 text-lg rounded-md hover:bg-opacity-80">
           ▶ Play
         </button>
         <button className=" mx-2 bg-gray-500 text-white p-2 px-16 text-lg rounded-md bg-opacity-60 ">
